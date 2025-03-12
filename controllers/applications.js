9da04ae2c54742f5115f195b1d3cd4fd447b1585
abl00mfield <amanda.bloomfield@gmail.com>
2025-03-12 16:25:01 -0600

    shows all applications on index page

--- controllers/applications.js
+++ controllers/applications.js
@@ -8,7 +8,27 @@ const User = require("../models/user.js");
 
 router.get("/", async (req, res) => {
   try {
-    res.render("applications/index.ejs");
+    const currentUser = await User.findById(req.session.user._id);
+    res.render("applications/index.ejs", {
+      applications: currentUser.applications,
+    });
+  } catch (error) {
+    console.log(error);
+    res.redirect("/");
+  }
+});
+
+router.get("/new", async (req, res) => {
+  res.render("applications/new.ejs");
+});
+
+//post /users/:userId/applications/new
+router.post("/", async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    currentUser.applications.push(req.body);
+    await currentUser.save();
+    res.redirect("/users/${currentUser._id}/applications");
   } catch (error) {
     console.log(error);
     res.redirect("/");
